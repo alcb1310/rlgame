348aa7484ac2b7d11b1ae12ef3102eb44df5b7a7
Andres Court <andres@andrescourt.com>
2024-06-24 23:26:03 -0500

    Game Title

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"image/color"
 
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+var (
+	BG_COLOR   = color.RGBA{207, 255, 240, 0}
+	TEXT_COLOR = color.RGBA{105, 105, 255, 255}
+	CHAR_COLOR = color.RGBA{255, 161, 84, 255}
+)
+
+type Game struct {
+	Width    int32
+	Height   int32
+	FontSize int32
+
+}
 func main() {
 	fmt.Println("Hello, World!")
+	game := &Game{}
+	game.Init()
+
+	rl.InitWindow(game.Width, game.Height, "My cool game")
+	for !rl.WindowShouldClose() {
+		game.Draw()
+	}
+
+	rl.CloseWindow()
+}
+
+func (g *Game) Init() {
+	g.Width = 640
+	g.Height = 480
+	g.FontSize = 20
+}
+
+func (g *Game) Draw() {
+	welcomeText := "Congrats! You created your first window!"
+	rl.BeginDrawing()
+	rl.ClearBackground(BG_COLOR)
+	rl.DrawText(welcomeText, g.Width/2-rl.MeasureText(welcomeText, g.FontSize)/2, g.Height/2, g.FontSize, TEXT_COLOR)
+
+	rl.EndDrawing()
 }
