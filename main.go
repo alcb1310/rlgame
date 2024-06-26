2849b40a4649c773cd7015bb9fde032c159a3379
Andres Court <andres@andrescourt.com>
2024-06-26 18:49:20 -0500

    Fall to a floor surface

--- main.go
+++ main.go
@@ -19,9 +19,18 @@ type Game struct {
 
 	Character         Character
 	CharacterPosition rl.Vector2
+
+	Floor []Floor
 }
 
 type Character struct {
+	Position rl.Vector2
+	Size     rl.Vector2
+	Color    rl.Color
+	Speed    rl.Vector2
+}
+
+type Floor struct {
 	Position rl.Vector2
 	Size     rl.Vector2
 	Color    rl.Color
@@ -31,8 +40,11 @@ func main() {
 	game := &Game{}
 	game.Init()
 
+	rl.SetTargetFPS(30)
+
 	rl.InitWindow(game.Width, game.Height, "My cool game")
 	for !rl.WindowShouldClose() {
+		game.Fall()
 		game.Draw()
 	}
 
@@ -43,6 +55,11 @@ func (g *Game) Init() {
 	g.Width = 640
 	g.Height = 480
 	g.FontSize = 20
+	g.Character.Position = rl.Vector2{X: 10, Y: 0}
+	g.Character.Size = rl.Vector2{X: 20, Y: 20}
+	g.Character.Color = CHAR_COLOR
+	g.Character.Speed = rl.Vector2{X: 0, Y: 5}
+	g.Floor = []Floor{}
 }
 
 func (g *Game) Draw() {
@@ -51,12 +68,56 @@ func (g *Game) Draw() {
 	rl.ClearBackground(BG_COLOR)
 	rl.DrawText(welcomeText, g.Width/2-rl.MeasureText(welcomeText, g.FontSize)/2, g.Height/2, g.FontSize, TEXT_COLOR)
 
-	g.Character = Character{
-		Position: rl.Vector2{X: 0, Y: 0},
-		Color:    CHAR_COLOR,
-		Size:     rl.Vector2{X: 20, Y: 20},
-	}
 	rl.DrawRectangleV(g.Character.Position, g.Character.Size, g.Character.Color)
 
+	g.Character.Position = rl.Vector2{X: g.Character.Position.X, Y: g.Character.Position.Y}
+	fl := Floor{
+		Position: rl.Vector2{X: 0, Y: 400},
+		Size:     rl.Vector2{X: 100, Y: 10},
+		Color:    rl.Color{R: 0, G: 0, B: 0, A: 255},
+	}
+
+	fl1 := Floor{
+		Position: rl.Vector2{X: 100, Y: 350},
+		Size:     rl.Vector2{X: 100, Y: 10},
+		Color:    rl.Color{R: 0, G: 0, B: 0, A: 255},
+	}
+	g.Floor = append(g.Floor, fl)
+	g.Floor = append(g.Floor, fl1)
+
+	for _, f := range g.Floor {
+		rl.DrawRectangleV(f.Position, f.Size, f.Color)
+	}
+
+	// collisions
+	for _, floor := range g.Floor {
+		if rl.CheckCollisionRecs(
+			rl.Rectangle{
+				X:      g.Character.Position.X,
+				Y:      g.Character.Position.Y,
+				Width:  g.Character.Size.X,
+				Height: g.Character.Size.Y,
+			},
+			rl.Rectangle{
+				X:      floor.Position.X,
+				Y:      floor.Position.Y - floor.Size.Y/2,
+				Width:  floor.Size.X,
+				Height: floor.Size.Y,
+			},
+		) {
+			g.Character.Position = rl.Vector2{
+				X: g.Character.Position.X,
+				Y: floor.Position.Y - g.Character.Size.Y - floor.Size.Y/2,
+			}
+		}
+	}
+
 	rl.EndDrawing()
+}
+
+func (g *Game) Fall() {
+	if int32(g.Character.Position.Y) <= g.Height {
+		g.Character.Position = rl.Vector2{X: g.Character.Position.X, Y: g.Character.Position.Y + g.Character.Speed.Y}
+		g.Character.Position = rl.Vector2{X: g.Character.Position.X + g.Character.Speed.X, Y: g.Character.Position.Y}
+	}
 }
